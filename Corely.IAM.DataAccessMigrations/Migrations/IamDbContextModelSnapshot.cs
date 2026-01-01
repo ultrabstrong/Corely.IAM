87d5e2f2eff0b1ad7a9e3b528d9d177afda6baf0
Brandon <ultrabstrong@gmail.com>
2026-01-01 11:00:27 -0700

    Readd initial migration

--- Corely.IAM.DataAccessMigrations/Migrations/IamDbContextModelSnapshot.cs
+++ Corely.IAM.DataAccessMigrations/Migrations/IamDbContextModelSnapshot.cs
@@ -4,7 +4,6 @@ using Corely.IAM.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -12,11 +11,9 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Corely.IAM.DataAccessMigrations.Migrations
 {
     [DbContext(typeof(IamDbContext))]
-    [Migration("20260101171241_InitialMigration")]
-    partial class InitialMigration
+    partial class IamDbContextModelSnapshot : ModelSnapshot
     {
-        /// <inheritdoc />
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
